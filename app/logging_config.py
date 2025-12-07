5e14119da99cb54949488c01d5986856b26b8775
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-12-07 21:34:25 +0200

    feat: logging

---
+++ app/logging_config.py
@@ -0,0 +1,63 @@
+from logging.config import dictConfig
+
+from app.core.config import DevConfig, config
+
+### skipped: logging to Logtail
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "filters": {
+                "correlation_id": {
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                    "default_value": "-",
+                },
+            },
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y:%m:%dT%H:%M:%S",
+                    "format": "(%(correlation_id)s)  %(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    # "class": "logging.Formatter",
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",  # for storing logs as json , (i.e: mostly for storing somewhere external)
+                    "datefmt": "%Y:%m:%dT%H:%M:%S",
+                    # "format": "%(asctime)s.%(msecs)0dZ | %(levelname)-8s | [%(correlation_id)s] %(name)s:%(lineno)d - %(message)s",
+                    "format": "%(asctime)s.%(msecs)0dZ  %(levelname)-8s [%(correlation_id)s] %(name)s:%(lineno)d - %(message)s",  # with json
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",  # rich handler for nice logging.
+                    "level": "DEBUG",
+                    "formatter": "console",
+                    "filters": ["correlation_id"],
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "app.log",
+                    "maxBytes": 1024 * 1024,  # 1MB,
+                    "backupCount": 2,  # keep only 2 log files
+                    "encoding": "utf8",
+                    "filters": ["correlation_id"],
+                },
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
+                "app": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                },
+                "database": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            },
+        }
+    )
--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
+from contextlib import asynccontextmanager
+
+from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI
 
 from app.api.main import api_router
+from app.logging_config import configure_logging
 
-app = FastAPI()
 
-app.include_router(api_router)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    configure_logging()
+    yield
+
+
+app = FastAPI(
+    title="Spatial Agronomic Geo Engine API",
+    summary="""
+    A backend that ingests geospatial data about agricultural land parcel,
+    processes it into useful agricultural insights, stores the results,
+    and exposes them via REST APIs for internal tools, dashboards, or other services.
+    """,
+    lifespan=lifespan,
+)
+
+app.add_middleware(CorrelationIdMiddleware)
+
+app.include_router(api_router, prefix="/api")
