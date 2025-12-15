ca4e387d142b9e1bf06de1d52785220d6704f480
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-12-15 13:25:34 +0200

    test timeseries stats

--- tests/routes/test_parcels.py
+++ tests/routes/test_parcels.py
@@ -5,7 +5,7 @@ api_url_prefix = "/api/v1"
 
 
 async def create_parcel(payload: dict, async_client: AsyncClient) -> dict:
-    response = await async_client.post(f"{api_url_prefix}/parcels/", json=payload)
+    response = await async_client.post(f"{api_url_prefix}/parcels", json=payload)
     return response.json()
 
 
@@ -54,7 +54,7 @@ async def test_create_parcel(async_client: AsyncClient):
         "soil_type": "sandy loam",
         "irrigation_type": "mixed",
     }
-    response = await async_client.post(f"{api_url_prefix}/parcels/", json=payload)
+    response = await async_client.post(f"{api_url_prefix}/parcels", json=payload)
     assert response.status_code == 201
     assert {
         "name": "Nyamata Block A",
@@ -63,9 +63,11 @@ async def test_create_parcel(async_client: AsyncClient):
 
 
 @pytest.mark.anyio
-async def test_parcel_stats(async_client: AsyncClient, created_parcel: created_parcel):
+async def test_get_raw_parcel_stats(
+    async_client: AsyncClient, created_parcel: created_parcel
+):
     response = await async_client.get(
-        f"{api_url_prefix}/parcels/{created_parcel['uid']}/stats"
+        f"{api_url_prefix}/{created_parcel['uid']}/raw-stats"
     )
     assert response.status_code == 200
     assert {
@@ -76,13 +78,23 @@ async def test_parcel_stats(async_client: AsyncClient, created_parcel: created_p
 
 @pytest.mark.anyio
 async def test_get_parcel(async_client: AsyncClient, created_parcel: created_parcel):
-    response = await async_client.get(
-        f"{api_url_prefix}/parcels/{created_parcel['uid']}"
-    )
+    response = await async_client.get(f"{api_url_prefix}/{created_parcel['uid']}")
     assert response.status_code == 200
 
 
 @pytest.mark.anyio
 async def test_get_parcel_not_exists(async_client: AsyncClient):
-    response = await async_client.get(f"{api_url_prefix}/parcels/12344")
+    response = await async_client.get(f"{api_url_prefix}/12344")
     assert response.status_code == 404
+
+
+@pytest.mark.anyio
+async def test_get_timeseries_parcel_stats(
+    async_client: AsyncClient, created_parcel: created_parcel
+):
+    response = await async_client.get(f"{api_url_prefix}/{created_parcel['uid']}/stats")
+    assert response.status_code == 200
+    assert {
+        "parcel_id": created_parcel["uid"],
+        "stats": [],
+    }.items() <= response.json().items()
