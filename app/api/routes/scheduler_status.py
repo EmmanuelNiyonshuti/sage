78f578aff81e8335c17ec6249fc9258488583fa9
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-12-23 20:27:26 +0200

    chore: update job status check

--- app/api/routes/scheduler_status.py
+++ app/api/routes/scheduler_status.py
@@ -6,17 +6,8 @@ from app.scheduler.time_series_scheduler import time_series_scheduler
 router = APIRouter(tags=["Scheduler"])
 
 
-@router.get("/ping-scheduler")
-def health_check():
-    return {
-        "status": "pong",
-        "ingestion_scheduler_running": ingestion_scheduler.is_running(),
-        "timeseries_scheduler_running": time_series_scheduler.is_running(),
-    }
-
-
-@router.get("/ingestion_scheduler/status")
-def scheduler_status():
+@router.get("/scheduler/status")
+def scheduler_jobs_status():
     ingestion_jobs = ingestion_scheduler.get_jobs()
     time_series_jobs = time_series_scheduler.get_jobs()
     ingestion_jobs.extend(time_series_jobs)
