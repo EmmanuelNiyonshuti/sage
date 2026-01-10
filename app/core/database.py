b2bf01cfb12021da5cd4cbd2c862daa3ac1103aa
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-01-10 21:40:38 +0200

    chore: remove unused session factory func

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,5 @@
-from contextlib import contextmanager
-from typing import Generator
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from .config import config
 
@@ -13,19 +10,3 @@ session_factory = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
     pass
-
-
-@contextmanager
-def get_db_context() -> Generator[Session, None, None]:
-    """
-    provide database sessions, to be used when a caller wants a single session for a block of code.
-    """
-    session = session_factory()
-    try:
-        yield session
-        session.commit()
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
