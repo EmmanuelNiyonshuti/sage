9dc0f781637d9e65ff49940413dde25a0c49f346
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-03-18 11:48:01 +0200

    add user register route

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import parcels, raster_stats, scheduler_status, time_series
+from app.api.routes import parcels, raster_stats, scheduler_status, time_series, users
 
 api_router = APIRouter(prefix="/api/v1")
 
 
+api_router.include_router(users.router)
 api_router.include_router(parcels.router)
 api_router.include_router(raster_stats.router)
 api_router.include_router(scheduler_status.router)
